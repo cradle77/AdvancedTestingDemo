babb24e019dbf92fcde8846277b4e2c0afe18d6b
MarcoDes <marcodesanctis2@gmail.com>
2021-09-22 22:06:09 +0100

    loanservice test

---
+++ BankApi.Tests/Web/LoanServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using BankApi.Services;
+using BankApi.ViewModels;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankApi.Tests.Web
+{
+    public class LoanServiceTest 
+    {
+       
+    }
+}
